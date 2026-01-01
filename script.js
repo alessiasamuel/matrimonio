64c5de48b0d99b9772172937b7605a87b6dda7c0
alessiasamuel <giordanalessia95@gmail.com>
2026-01-01 19:33:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -17,10 +17,11 @@ function updateCountdown() {
   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   document.getElementById("days").innerText = days;
-  document.getElementById("hours").innerText = hours;
-  document.getElementById("minutes").innerText = minutes;
-  document.getElementById("seconds").innerText = seconds;
+  #document.getElementById("hours").innerText = hours;
+  #document.getElementById("minutes").innerText = minutes;
+  #document.getElementById("seconds").innerText = seconds;
 }
 
 setInterval(updateCountdown, 1000);
 updateCountdown();
+
